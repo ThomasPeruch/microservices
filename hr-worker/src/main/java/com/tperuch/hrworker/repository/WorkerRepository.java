26ff1949da9982a988af3753dfd80aab23674a3c
ThomasPeruch <peruchthomas@gmail.com>
2021-08-10 16:25:17 -0300

    created resources package

--- hr-worker/src/main/java/com/tperuch/hrworker/repository/WorkerRepository.java
+++ hr-worker/src/main/java/com/tperuch/hrworker/repository/WorkerRepository.java
@@ -3,4 +3,5 @@ package com.tperuch.hrworker.repository;
 import com.tperuch.hrworker.entities.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkerRepository extends JpaRepository<Worker, Long> {}
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
---
+++ hr-worker/src/main/java/com/tperuch/hrworker/resources/WorkerResource.java
@@ -0,0 +1,26 @@
+package com.tperuch.hrworker.resources;
+
+import com.tperuch.hrworker.entities.Worker;
+import com.tperuch.hrworker.repository.WorkerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+@AllArgsConstructor
+public class WorkerResource {
+
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
