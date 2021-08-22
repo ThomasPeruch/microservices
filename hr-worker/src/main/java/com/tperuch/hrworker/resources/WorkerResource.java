b0f16257513fd44b5b45ece428f3ae5078611e0f
ThomasPeruch <peruchthomas@gmail.com>
2021-08-21 22:56:17 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/tperuch/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/tperuch/hrworker/resources/WorkerResource.java
@@ -3,6 +3,9 @@ package com.tperuch.hrworker.resources;
 import com.tperuch.hrworker.entities.Worker;
 import com.tperuch.hrworker.service.WorkerService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,17 +19,20 @@ import java.util.List;
 @AllArgsConstructor
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+    private Environment environment;
     private WorkerService service;
 
     @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = service.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
-        Worker entity = service.findById(id);
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT = {}", environment.getProperty("local.server.port"));
+                Worker entity = service.findById(id);
         return ResponseEntity.ok(entity);
     }
 }
