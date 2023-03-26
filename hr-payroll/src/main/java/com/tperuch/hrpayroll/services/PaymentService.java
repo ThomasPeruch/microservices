946e8453747b1ba93e437a1b5f4200e8fbca3dc6
Thomas Peruch <peruchthomas@gmail.com>
2023-03-26 19:59:08 -0300

    feat: communication between hr-payroll and hr-workers by rest template

--- hr-payroll/src/main/java/com/tperuch/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/tperuch/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.tperuch.hrpayroll.services;
 
 import com.tperuch.hrpayroll.models.Payment;
+import com.tperuch.hrpayroll.models.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days){
-         return new Payment("Cris", 500.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = restTemplate.getForObject(workerHost +"/workers/{id}/", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
