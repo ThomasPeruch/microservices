aa314b02a9e881f1fed8736dd997e4986b172468
Thomas Peruch <peruchthomas@gmail.com>
2023-04-01 23:03:59 -0300

    feat: add Hystrix fault tolerance

--- hr-payroll/src/main/java/com/tperuch/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/tperuch/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.tperuch.hrpayroll.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.tperuch.hrpayroll.models.Payment;
 import com.tperuch.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlteranative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlteranative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+
     }
 }
