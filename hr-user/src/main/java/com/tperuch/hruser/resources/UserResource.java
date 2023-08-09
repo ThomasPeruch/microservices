d0d5d64a7d73cc0b83d5a4dd40b85c339c821c77
Thomas Peruch <peruchthomas@gmail.com>
2023-08-09 16:16:19 -0300

    find user by email resource implemented

--- hr-user/src/main/java/com/tperuch/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/tperuch/hruser/resources/UserResource.java
@@ -4,10 +4,9 @@ import com.tperuch.hruser.entities.UserEntity;
 import com.tperuch.hruser.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.QueryParam;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,6 +18,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserEntity> findById(@PathVariable Long id){
         UserEntity userEntity = userRepository.findById(id).get();
+        return ResponseEntity.ok(userEntity);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<UserEntity> findById(@RequestParam String email){
+        UserEntity userEntity = userRepository.findByEmail(email);
         return ResponseEntity.ok(userEntity);
     }
 }
